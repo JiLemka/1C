85c12660a6f2445d60f97a14ce4359b791dae961
JiLemka <antonmefedov@icloud.com>
2022-07-20 18:11:03 +0300

    fix commit

--- summary.py
+++ summary.py
@@ -1,18 +1,24 @@
 inp = open("access.log", 'r')
-summary_mass = list()
 numbers = inp.read()
 numbers = numbers.split(sep="\n")
-
 summary = 0
+summary_mas = ""
+
 for i in range(len(numbers)):
     numbers_1 = list(numbers[i])
     numbers_1 = numbers_1[::-1]
+    summary_mas += " "
     for j in range(len(numbers_1)):
         if numbers_1[j] != " ":
-            summary += int(numbers_1[j])
+            summary_mas += numbers_1[j]
         else:
             break
 
-print(summary)
+summary_mas = summary_mas[::-1]
+summary_mas = summary_mas.split()
 
+for i in range(len(summary_mas)):
+    summary += int(summary_mas[i])
+
+print(summary)
 inp.close()
